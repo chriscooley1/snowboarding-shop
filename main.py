215db3c4139616868a52f063b0bd334c3a489318
Chris Cooley <ccooley@bonadmin.com>
2024-04-12 08:51:41 -0600

    add functionality to snowboards api

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 import json
 
+from fastapi import FastAPI
+
+from models import Snowboard, Brand
+
+
+app = FastAPI()
+
 
 with open("snowboards.json", "r") as f:
     snowboard_list = json.load(f)
+
+snowboards: list[Snowboard] = []
+
+for snowboard in snowboard_list:
+    snowboards.append(Snowboard(**snowboard))
+
+
+@app.get("/snowboards")
+async def get_snowboards() -> list[Snowboard]:
+    return snowboards
+
+@app.post("/snowboards")
+async def create_snowboards(snowboard: Snowboard) -> None:
+    snowboards.append(snowboard)
+
+@app.put("/snowboards")
+async def update_snowboards(id: int, updated_snowboard: Snowboard) -> None:
+    for i, snowboard in enumerate(snowboards):
+        if snowboard.id == id:
+            snowboards[i] = updated_snowboard
+            return
+    
+@app.delete("/snowboards")
+async def delete_snowboards(id: int) -> None:
+    for i, snowboard in enumerate(snowboards):
+        if snowboard.id == id:
+            snowboards.pop(i)
+            return
